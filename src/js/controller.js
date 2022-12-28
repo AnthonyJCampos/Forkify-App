5bf66f335c737e17f8c554de9b6357dc910484f8
Anthony Campos <Anthonyck89@hotmail.com>
2022-12-27 17:16:02 -0800

    testing API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+async function showRecipe() {
+  try {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(`${data.message} (${response.status})`);
+    }
+    console.log(response, data);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (error) {
+    alert(error);
+  }
+} // end showRecipe
+
+showRecipe();
